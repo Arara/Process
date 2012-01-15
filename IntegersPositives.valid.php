26f144ff2e4b8f7f209b8ec5441b8b1c325ce241
Henrique Moody <henriquemoody@gmail.com>
2012-01-15 02:45:21 -0200

    Update IntegersPositives.valid.php

--- IntegersPositives.valid.php
+++ IntegersPositives.valid.php
@@ -10,6 +10,8 @@ return array_map(
     function ($item) {
         if ($item[0] < 0) {
             $item[0] *= -1;
+        } elseif ($item[0] < 0) {
+            $item[0] = rand(1,500);
         }
         return $item;
     }, 
