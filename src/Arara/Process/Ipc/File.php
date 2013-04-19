6949180d3da5f9814dde6bfe251514df4263191e
Henrique Moody <henriquemoody@gmail.com>
2013-04-19 20:40:16 -0300

    Created an IPC using file

---
+++ src/Arara/Process/Ipc/File.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Arara\Process\Ipc;
+
+class File implements Ipc
+{
+
+    private $filename;
+
+    public function __construct($dirname = null)
+    {
+        if (null === $dirname) {
+            $dirname = sys_get_temp_dir();
+        }
+
+        if (!is_string($dirname) || (is_string($dirname) && !is_dir($dirname))) {
+            $message = '"%s" is not a valid directory';
+            throw new \InvalidArgumentException(sprintf($message, print_r($dirname, true)));
+        }
+
+        if (!is_writable($dirname)) {
+            $message = '"%s" is not writable';
+            throw new \InvalidArgumentException(sprintf($message, print_r($dirname, true)));
+        }
+
+        $filename = $dirname . '/' . uniqid('arara_') . '.ipc';
+
+        touch($filename);
+        chmod($filename, 0777);
+
+        $this->filename = $filename;
+    }
+
+    public function destroy()
+    {
+        unlink($this->filename);
+    }
+
+    public function getData()
+    {
+        $content    = file_get_contents($this->filename);
+        $data       = @unserialize($content);
+        if (false === $data) {
+            return array();
+        }
+
+        return $data;
+    }
+
+    public function save($name, $value)
+    {
+        $data = $this->getData();
+        $data[$name] = $value;
+
+        file_put_contents($this->filename, serialize($data), LOCK_EX);
+
+        return $this;
+    }
+
+    public function load($name)
+    {
+        $data = $this->getData();
+        $value = null;
+        if (isset($data[$name])) {
+            $value = $data[$name];
+        }
+
+        return $value;
+    }
+
+}
---
+++ tests/Arara/Process/Ipc/FileTest.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Arara\Process\Ipc;
+
+function uniqid($prefix = null) { return $prefix . $GLOBALS['uniqid']; }
+function touch($filename) { $GLOBALS['touch'] = $filename; }
+function unlink($filename) { $GLOBALS['unlink'] = $filename; }
+function chmod($filename, $mode) { $GLOBALS['chmod_filename'] = $filename; $GLOBALS['chmod_mode'] = $mode; }
+function is_dir() { return $GLOBALS['is_dir']; }
+function is_writable() { return $GLOBALS['is_writable']; }
+function file_get_contents() { return @$GLOBALS['file_contents']; }
+function file_put_contents($filename, $content) { $GLOBALS['file_contents'] = $content; }
+
+class FileTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected function setUp()
+    {
+        $GLOBALS['uniqid'] = '39F';
+        $GLOBALS['is_dir'] = true;
+        $GLOBALS['is_writable'] = true;
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage "/root" is not a valid directory
+     * @covers Arara\Process\Ipc\File::__construct
+     */
+    public function testShoudThrowsAnExceptionIfGivenDiretoryIsNotValid()
+    {
+        $GLOBALS['is_dir'] = false;
+
+        new File('/root');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage "/root" is not writable
+     * @covers Arara\Process\Ipc\File::__construct
+     */
+    public function testShoudThrowsAnExceptionIfGivenDiretoryIsNotWritable()
+    {
+        $GLOBALS['is_writable'] = false;
+
+        new File('/root');
+    }
+
+    /**
+     * @covers Arara\Process\Ipc\File::__construct
+     */
+    public function testShoudCreateFile()
+    {
+        $ipc = new File('/root');
+        $filename = '/root/arara_' . $GLOBALS['uniqid'] . '.ipc';
+
+        $this->assertSame($filename, $GLOBALS['touch']);
+        $this->assertSame($filename, $GLOBALS['chmod_filename']);
+        $this->assertSame(0777, $GLOBALS['chmod_mode']);
+        $this->assertAttributeSame($filename, 'filename', $ipc);
+    }
+
+    /**
+     * @covers Arara\Process\Ipc\File::__construct
+     * @depends testShoudCreateFile
+     */
+    public function testShouldUseTemporaryDirectoryByDefault()
+    {
+        $ipc = new File();
+        $filename = sys_get_temp_dir() . '/arara_' . $GLOBALS['uniqid'] . '.ipc';
+
+        $this->assertSame($filename, $GLOBALS['touch']);
+    }
+
+
+    /**
+     * @covers Arara\Process\Ipc\File::destroy
+     */
+    public function testShoudDestroyFile()
+    {
+        $ipc = new File('/root');
+        $ipc->destroy();
+
+        $filename = '/root/arara_' . $GLOBALS['uniqid'] . '.ipc';
+
+        $this->assertSame($filename, $GLOBALS['unlink']);
+    }
+
+    /**
+     * @covers Arara\Process\Ipc\File::getData
+     */
+    public function testShoudReturnAllDataFile()
+    {
+        $ipc = new File('/root');
+
+        $data = array(
+            'foo' => 123,
+            'bar' => 456,
+            'baz' => 789,
+        );
+
+        $GLOBALS['file_contents'] = serialize($data);
+
+
+        $this->assertSame($data, $ipc->getData());
+    }
+
+    /**
+     * @covers Arara\Process\Ipc\File::getData
+     * @depends testShoudReturnAllDataFile
+     */
+    public function testShoudReturnAnEmptyArrayIfFileDataIsNotValid()
+    {
+        $ipc = new File('/root');
+
+        $data = array(
+            'foo' => 123,
+            'bar' => 456,
+            'baz' => 789,
+        );
+
+        $GLOBALS['file_contents'] = __DIR__ . serialize($data);
+
+        $this->assertSame(array(), $ipc->getData());
+    }
+
+    /**
+     * @covers Arara\Process\Ipc\File::save
+     * @depends testShoudReturnAllDataFile
+     */
+    public function testShouldSaveData()
+    {
+        $ipc = new File('/root');
+        $ipc->save('foo', 123);
+        $ipc->save('bar', 456);
+
+        $this->assertSame(
+            array('foo' => 123, 'bar' => 456),
+            unserialize($GLOBALS['file_contents'])
+        );
+    }
+    /**
+     * @covers Arara\Process\Ipc\File::load
+     * @depends testShouldSaveData
+     */
+    public function testShouldLoadData()
+    {
+        $ipc = new File('/root');
+        $ipc->save('foo', 123);
+
+        $this->assertSame(123, $ipc->load('foo'));
+    }
+
+}
