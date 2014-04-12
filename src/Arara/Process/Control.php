327b71dcbb8d1006ddfded8f40c70ce4267f5423
Henrique Moody <henriquemoody@gmail.com>
2014-04-12 00:06:50 -0300

    Create process control class

---
+++ src/Arara/Process/Control.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Arara\Process;
+
+use RuntimeException;
+
+class Control
+{
+    /**
+     * @link   http://php.net/pcntl_exec
+     * @param  string $path
+     * @param  array[optional] $args
+     * @param  array[optional] $envs
+     */
+    public function execute($path, array $args = array(), array $envs = array())
+    {
+        if (false === @pcntl_exec($path, $args, $envs)) {
+            throw new RuntimeException('Error when executing command');
+        }
+    }
+
+    // @codeCoverageIgnoreStart
+    /**
+     * This method exists to allow us to test without quit the program.
+     *
+     * @SuppressWarnings("exit")
+     *
+     * @link   http://php.net/exit
+     * @param  int[optional] $exitCode
+     */
+    public function quit($exitCode = 0)
+    {
+        exit($exitCode);
+    }
+    // @codeCoverageIgnoreEnd
+
+    /**
+     * @link   http://php.net/pcntl_fork
+     * @throws RuntimeException When fork fails.
+     * @return int When is the child process returns "0" unless returns the child PID.
+     */
+    public function fork()
+    {
+        $processId = @pcntl_fork();
+        if ($processId === -1) {
+            throw new RuntimeException('Unable to fork process');
+        }
+
+        return $processId;
+    }
+
+    /**
+     * @link   http://php.net/pcntl_wait
+     * @param  int[optional] $status
+     * @param  int[optional] $options
+     * @return int
+     */
+    public function wait(&$status = null, $options = 0)
+    {
+        return pcntl_wait($status, $options);
+    }
+
+    /**
+     * @link   http://php.net/pcntl_waitpid
+     * @return int
+     */
+    public function waitProcessId($processId, &$status = null, $options = 0)
+    {
+        return pcntl_waitpid($processId, $status, $options);
+    }
+}
---
+++ tests/Arara/Process/ControlTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Arara\Process;
+
+/**
+ * @covers Arara\Process\Control
+ */
+class ControlTest extends \TestCase
+{
+    public function testShouldExcecuteACommand()
+    {
+        $command = '/bin/date';
+
+        $control = new Control();
+        $control->execute($command);
+
+        $this->assertEquals($command, $GLOBALS['arara']['pcntl_exec']['args'][0]);
+    }
+
+    /**
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage Error when executing command
+     */
+    public function testShouldThrowsAnExceptionWhenCommandExecutionFails()
+    {
+        $GLOBALS['arara']['pcntl_exec']['return'] = false;
+
+        $control = new Control();
+        $control->execute('/bin/date');
+    }
+
+    public function testShouldReturnPidForParentProcessWhenForking()
+    {
+        $GLOBALS['arara']['pcntl_fork']['return'] = 12345;
+
+        $control = new Control();
+
+        $this->assertEquals($GLOBALS['arara']['pcntl_fork']['return'], $control->fork());
+    }
+
+    public function testShouldReturnZeroForChildProcessWhenForking()
+    {
+        $GLOBALS['arara']['pcntl_fork']['return'] = 0;
+
+        $control = new Control();
+
+        $this->assertEquals($GLOBALS['arara']['pcntl_fork']['return'], $control->fork());
+    }
+
+    /**
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage Unable to fork process
+     */
+    public function testShouldThrowsAnExceptionWhenForkFails()
+    {
+        $GLOBALS['arara']['pcntl_fork']['return'] = -1;
+
+        $control = new Control();
+        $control->fork();
+    }
+
+    public function testShouldWaitAndReturn()
+    {
+        $GLOBALS['arara']['pcntl_wait']['return'] = -1;
+
+        $control = new Control();
+        $this->assertEquals(-1, $control->wait());
+    }
+
+    public function testShouldWaitAndUpdateStatus()
+    {
+        $GLOBALS['arara']['pcntl_wait']['status'] = 42;
+
+        $control = new Control();
+        $status = null;
+        $control->wait($status);
+
+        $this->assertEquals(42, $status);
+    }
+
+    public function testShouldWaitProcessId()
+    {
+        $control = new Control();
+        $control->waitProcessId(999);
+
+        $this->assertEquals(array(999, null, 0), $GLOBALS['arara']['pcntl_waitpid']['args']);
+    }
+
+    public function testShouldWaitProcessIdAndReturn()
+    {
+        $GLOBALS['arara']['pcntl_waitpid']['return'] = -1;
+
+        $control = new Control();
+        $this->assertEquals(-1, $control->waitProcessId(999));
+    }
+
+    public function testShouldWaitProcessIdAndUpdateStatus()
+    {
+        $GLOBALS['arara']['pcntl_waitpid']['status'] = 42;
+
+        $control = new Control();
+        $status = null;
+        $control->waitProcessId(999, $status);
+
+        $this->assertEquals(42, $status);
+    }
+}
